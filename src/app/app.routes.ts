ab85ac49fcf0bc29c997b1d5ed9023d15b304e3a
randriamiharyDani <randriamiharySteev@gmail.com>
2024-09-18 23:33:50 +0300

    fieature: dashboard composant et css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { BudgerPlannerComponent } from './budger-planner/budger-planner.component';
 import { LoginComponent } from './budger-planner/login/login.component';
 import { SideNavComponent } from './budger-planner/side-nav/side-nav.component';
+import { DashboardComponent } from './budger-planner/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
@@ -20,6 +21,10 @@ export const routes: Routes = [
       {
         path: 'side-nav',
         component : SideNavComponent
+      },
+      {
+        path : 'dashboard',
+        component : DashboardComponent
       },
 
     ],
---
+++ src/app/budger-planner/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { SideNavComponent } from "../side-nav/side-nav.component";
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [MatIconModule, SideNavComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+
+}
